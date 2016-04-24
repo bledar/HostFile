d26e268c80a2c0ff78297e170322d4341cb3d7f6
bledar <bledi-1@live.com>
2016-04-24 20:36:04 +0200

    Init

---
+++ src/MainHost.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MainHost {
+
+	public static void main(String[] args) {
+		Nderfaqja n=new Nderfaqja();
+		n.setDritare(800,700);
+	}
+	
+}
