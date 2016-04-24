aa54780908ee5d50c52b529f0b10e7bcf9a39d2d
bledar <bledi-1@live.com>
2016-04-24 22:47:28 +0200

    Hapet nderfaqja por nuk mund te fshijme nga lista

--- src/Windows.java
+++ src/Windows.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -13,8 +15,8 @@ public class Windows {
 	JPanel trupi=new JPanel();
 	JList lista;
 	JLabel teksti= new JLabel("Shtoni nje host!");
-	//LexoHost host=new LexoHost(); 
-	
+	ManipulateHost host=new ManipulateHost(); 
+	DefaultListModel model;
 	void setDritare(int gj ,int l){
 		dritare.setVisible(true);	
 		dritare.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,19 +29,32 @@ public class Windows {
 		trupi.setLayout(null);
 		trupi.setBackground( new Color(200,200,200));
 		dritare.add(trupi);
-//		host.setPath(path);
-//		host.openFile();
-//		host.readFile();
-//		host.closeFile();
-		//System.out.println(host.getLista().get(0));
-		//lista = new JList<Object>(host.getLista().toArray());
-		//int h=host.getLista().size();
-		//lista.setBackground( new Color(222,222,222));
-		//lista.setBounds(15,15,300,h*20);
-		//lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		//trupi.add(lista);
+		host.setPath(path);
+		host.openFile();
+		host.readHost();
+		host.closeFile();
+		//System.out.println(host.getList().get(0));
+		int h=host.getList().size();
+		lista.setBackground( new Color(222,222,222));
+		lista.setBounds(15,15,300,h*20);
+		lista.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		trupi.add(lista);
+		trupi.repaint();
+		this.setFshije();
+	}
+	
+	public void setFshije(){
 		fshije.setBounds(325, 15, 120,30);
 		trupi.add(fshije);
-		trupi.repaint();
+		fshije.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){ 
+				int index = lista.getSelectedIndex();
+				System.out.println(index);
+				//lista.remove();
+				//lista.repaint();
+			}	
+			
+		});
 	}
 }
